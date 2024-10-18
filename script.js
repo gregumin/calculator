7e414c06da274a5f7f6bf3a2f3cd4fd4dec65d5f
gregoriusjeasenridzal@gmail.com <gregoriusjeasenridzal@gmail.com>
2024-10-18 14:28:00 +0800

    Add operate function
    Add an overarching function "operate" that accepts 2 values and a text that serves to designate an operator to decide which of the other 4 functions to call.

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-let add = function(a,b) {return a + b};
-let subtract = function(a,b) {return a-b};
-let multiple = function(a,b) {return a*b};
-let divide = function(a,b) {return a/b};
+const add = function(a,b) {return a + b};
+const subtract = function(a,b) {return a-b};
+const multiple = function(a,b) {return a*b};
+const divide = function(a,b) {return a/b};
+
+let firstNum = 0;
+let secondNum = 0;
+let operator;
+
+let plus = "+";
+let minus = "-";
+let times = "*";
+let cut = "/";
+
+const operate = (firstNum, secondNum, operator) => {
+switch(operator){
+    case "+":
+        return add(firstNum,secondNum);
+        break;
+    case "-":
+        return subtract(firstNum,secondNum);
+        break;
+    case "*":
+        return multiply(firstNum,secondNum);
+        break;
+    case "/":
+        return divide(firstNum,secondNum);
+        break;
+    default:
+        return "unknown operation";
+}
+}
+
 
 
